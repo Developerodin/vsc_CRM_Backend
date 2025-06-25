ba8f2d2e6e952cad4ec3ed2cad3d84d32339b707
Devwardhan Kothari <88496527+YourLocalCafe@users.noreply.github.com>
2025-06-25 10:45:56 +0530

    working

--- src/validations/timeline.validation.js
+++ src/validations/timeline.validation.js
@@ -74,8 +74,8 @@ const createTimeline = {
     udin: Joi.string().trim().allow(''),
     turnover: Joi.number(),
     assignedMember: Joi.string().custom(objectId).required(),
-    startDate: Joi.string().pattern(/^\d{4}-\d{2}-\d{2}$/),
-    endDate: Joi.string().pattern(/^\d{4}-\d{2}-\d{2}$/),
+    startDate: Joi.date(),
+    endDate: Joi.date(),
   }).custom((value, helpers) => {
     try {
       validateFrequencyConfig(value.frequency, value.frequencyConfig);
